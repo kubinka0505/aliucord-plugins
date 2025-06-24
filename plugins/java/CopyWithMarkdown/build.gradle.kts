42efd46d87519b5b3b873284105a3f46ebe5b66e
Halkiion <ane200055@gmail.com>
2025-06-24 01:19:28 +0100

    Initial commit

---
+++ plugins/java/CopyWithMarkdown/build.gradle.kts
@@ -0,0 +1,12 @@
+version = "1.0.0"
+description = "Copy message with markdown! Use 'Separate buttons' setting to use this alongside Discord's default."
+
+aliucord {
+
+    changelog.set("""
+        1.0.0
+        - First release!
+    """.trimIndent())
+
+    excludeFromUpdaterJson.set(false)
+}
