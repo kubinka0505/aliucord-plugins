593bd68dfaaedf815772258024652072fe090d20
kubinka0505 <kubinka0505@yandex.com>
2025-07-20 12:45:38 +0200

    Update Freedom.kt

--- plugins/kotlin/Freedom/src/main/kotlin/com/github/kubinka/Freedom.kt
+++ plugins/kotlin/Freedom/src/main/kotlin/com/github/kubinka/Freedom.kt
@@ -7,31 +7,31 @@ import com.aliucord.patcher.after
 
 @AliucordPlugin
 class Freedom : Plugin() {
-    private val characters = mapOf(
-        'a' to 'ą',
-        'e' to 'ę',
+	private val characters = mapOf(
+		'a' to 'ą',
+		'e' to 'ę',
 		'c' to 'ć',
 		'l' to 'ł',
 		'n' to 'ń',
 		's' to 'ś',
 		'z' to 'ź'
-    )
+	)
 
-    private fun replaceCharacters(input: String): String {
-        return input.map { char -> characters[char] ?: char }.joinToString("")
-    }
+	private fun replaceCharacters(input: String): String {
+		return input.map { char -> characters[char] ?: char }.joinToString("")
+	}
 
-    override fun start(ctx: Context) {
-        val clazz = Class.forName("com.discord.models.domain.ModelMessageSendRequest")
+	override fun start(ctx: Context) {
+		val clazz = Class.forName("com.discord.models.domain.ModelMessageSendRequest")
 
-        patcher.after(clazz, "getContent", *arrayOf()) { call ->
-            val original = call.result as String
-            val modified = replaceCharacters(original)
-            call.setResult(modified)
-        }
-    }
+		patcher.after(clazz, "getContent", *arrayOf()) { call ->
+			val original = call.result as String
+			val modified = replaceCharacters(original)
+			call.setResult(modified)
+		}
+	}
 
-    override fun stop(ctx: Context) {
-        patcher.unpatchAll()
-    }
+	override fun stop(ctx: Context) {
+		patcher.unpatchAll()
+	}
 }
