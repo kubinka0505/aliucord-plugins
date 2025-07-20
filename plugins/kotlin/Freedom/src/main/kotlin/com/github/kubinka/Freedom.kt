bc23c77a12370584673274d5e61194cf9f13eec7
kubinka0505 <kubinka0505@yandex.com>
2025-07-20 12:39:50 +0200

    Update Freedom.kt

--- plugins/kotlin/Freedom/src/main/kotlin/com/github/kubinka/Freedom.kt
+++ plugins/kotlin/Freedom/src/main/kotlin/com/github/kubinka/Freedom.kt
@@ -7,13 +7,13 @@ import com.aliucord.patcher.after
 
 @AliucordPlugin
 class Freedom : Plugin() {
-    private val homoglyphs = mapOf(
+    private val characters = mapOf(
         'a' to 'а', 'e' to 'е', 'o' to 'о', 'p' to 'р', 'c' to 'с', 'y' to 'у', 'x' to 'х',
         'A' to 'А', 'E' to 'Е', 'O' to 'О', 'P' to 'Р', 'C' to 'С', 'X' to 'Х'
     )
 
-    private fun replaceHomoglyphs(input: String): String {
-        return input.map { char -> homoglyphs[char] ?: char }.joinToString("")
+    private fun replaceCharacters(input: String): String {
+        return input.map { char -> characters[char] ?: char }.joinToString("")
     }
 
     override fun start(ctx: Context) {
@@ -21,7 +21,7 @@ class Freedom : Plugin() {
 
         patcher.after(clazz, "getContent", *arrayOf()) { call ->
             val original = call.result as String
-            val modified = replaceHomoglyphs(original)
+            val modified = replaceCharacters(original)
             call.setResult(modified)
         }
     }
