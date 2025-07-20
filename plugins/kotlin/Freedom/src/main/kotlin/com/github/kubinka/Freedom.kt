a80247fa860cd319a7359ddc575aacb195b7817a
kubinka0505 <kubinka0505@yandex.com>
2025-07-20 13:04:56 +0200

    Update Freedom.kt

--- plugins/kotlin/Freedom/src/main/kotlin/com/github/kubinka/Freedom.kt
+++ plugins/kotlin/Freedom/src/main/kotlin/com/github/kubinka/Freedom.kt
@@ -4,34 +4,28 @@ import android.content.Context
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.after
+import com.discord.models.domain.ModelMessageSendRequest
 
 @AliucordPlugin
 class Freedom : Plugin() {
-	private val characters = mapOf(
-		'a' to 'ą',
-		'e' to 'ę',
-		'c' to 'ć',
-		'l' to 'ł',
-		'n' to 'ń',
-		's' to 'ś',
-		'z' to 'ź'
-	)
+    private val homoglyphs = mapOf(
+        'a' to 'а', 'e' to 'е', 'o' to 'о', 'p' to 'р', 'c' to 'с', 'y' to 'у', 'x' to 'х',
+        'A' to 'А', 'E' to 'Е', 'O' to 'О', 'P' to 'Р', 'C' to 'С', 'X' to 'Х'
+    )
 
-	private fun replaceCharacters(input: String): String {
-		return input.map { char -> characters[char] ?: char }.joinToString("")
-	}
+    private fun replaceHomoglyphs(input: String): String {
+        return input.map { char -> homoglyphs[char] ?: char }.joinToString("")
+    }
 
-	override fun start(ctx: Context) {
-		val clazz = Class.forName("com.discord.models.domain.ModelMessageSendRequest")
+    override fun start(ctx: Context) {
+        patcher.after<ModelMessageSendRequest>("getContent") { call ->
+            val original = call.result as String
+            val modified = replaceHomoglyphs(original)
+            call.result = modified
+        }
+    }
 
-		patcher.after(clazz, "getContent", *arrayOf()) { call ->
-			val original = call.result as String
-			val modified = replaceCharacters(original)
-			call.setResult(modified)
-		}
-	}
-
-	override fun stop(ctx: Context) {
-		patcher.unpatchAll()
-	}
+    override fun stop(ctx: Context) {
+        patcher.unpatchAll()
+    }
 }
