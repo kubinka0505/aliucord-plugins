d87a497feab9e6594eee6225d135a65b4036f2d6
kubinka0505 <kubinka0505@yandex.com>
2025-07-20 12:59:21 +0200

    Create build.gradle.kts

---
+++ plugins/java/Freedom/build.gradle.kts
@@ -0,0 +1,11 @@
+version = "1.0.0"
+description = "Express yourself."
+
+aliucord {
+    changelog.set("""
+        1.0.0
+        - Initial release.
+    """.trimIndent())
+
+    excludeFromUpdaterJson.set(false)
+}
